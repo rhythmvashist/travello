31fb38c9812e39a0da02409f13a1ece6e9bfa5ab
Rhythmvashist <vashistrhythm1@gmail.com>
2019-09-16 23:54:33 -0700

    setup postges and migrations

--- travelo/settings.py
+++ travelo/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'trvl.apps.TrvlConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -76,8 +77,11 @@ WSGI_APPLICATION = 'travelo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travello',
+        'USER':'postgres',
+        'PASSWORD':'rhythm',
+        'HOST':'localhost'
     }
 }
 
@@ -122,5 +126,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
-
 STATIC_ROOT=os.path.join(BASE_DIR,'assets')
--- trvl/views.py
+++ trvl/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
+from .models import Destination
 
 # Create your views here.
-
 def index(request):
-    return render(request,'index.html')
+    return render(request,'index.html',{'price':700,})
