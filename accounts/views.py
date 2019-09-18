757296d220aef0a3818400ef60012496ce1ea0af
Rhythmvashist <vashistrhythm1@gmail.com>
2019-09-17 22:12:09 -0700

    updated the register

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.models import User,auth
+from django.contrib import  messages
 
 # Create your views here.
 
@@ -12,10 +13,20 @@ def register(request):
         email=request.POST['email']
         username=request.POST['username']
 
-        user=User.objects.create_user(username=username,password=pass1,email=email,first_name=first,last_name=last)
-        user.save()
-        print('user created')
+        if pass1 ==pass2:
+            if User.objects.filter(username=username).exists():
+               messages.info (request,'user name taken')
+               return redirect('register')
+            elif  User.objects.filter(email=email).exists():
+                messages.info(request, 'E-mail taken')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,password=pass1,email=email,first_name=first,last_name=last)
+                user.save()
+                messages.info(request, 'User created')
+        else:
+            messages.info(request,"password not matching")
+            return redirect('register')
         return redirect('/')
-
     else:
         return render(request,'register.html',)
